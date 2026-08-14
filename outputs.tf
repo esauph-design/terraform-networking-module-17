5a09bbe9cd3b7b4011c1e34866a182f9e7e853a6
Esau Picado <esauph@gmail.com>
2026-08-14 10:34:35 -0600

    added vnet_id to outputs for usage in module 30

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "subnet_ids" {
   value = module.networking.subnet_ids
 }
+output "resource_group_name" {
+  value = module.networking.resource_group_name
+}
+output "vnet_id" {
+  value = module.networking.virtual_network_id
+}
